9e97ceb664c1d4e5be83cff85b6570681e51517e
Vinod Biradar <vinod.biradar@atherenergy.com>
2023-04-02 09:33:44 +0530

    added bpmn for booking flow and dmn for booking process decision

---
+++ src/main/java/com/example/workflow/camunda/service/booking/DestinationLocation.java
@@ -0,0 +1,25 @@
+package com.example.workflow.camunda.service.booking;
+
+import org.camunda.bpm.engine.delegate.BpmnError;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+import java.util.logging.Logger;
+
+public class DestinationLocation implements JavaDelegate {
+
+    private final Logger log = Logger.getLogger(DestinationLocation.class.getName());
+
+    @Override
+    public void execute(DelegateExecution execution) throws Exception {
+        try{
+            //call gupshup to send message
+            log.info("DestinationLocation: execute method is called......");
+            //set relevant variables for future ref
+            execution.setVariable("isValidated", true);
+        } catch (Exception e){
+            log.warning("DestinationLocation: Exception occured......");
+            throw new BpmnError("booking_flow_error","Error sending message.....");
+        }
+    }
+}
