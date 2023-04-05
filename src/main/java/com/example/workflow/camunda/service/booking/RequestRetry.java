8804210e116b36fc93cfe39f715309d33e7ed284
Vinod Biradar <vinod.biradar@atherenergy.com>
2023-04-05 23:59:59 +0530

    added retry selection flow

---
+++ src/main/java/com/example/workflow/camunda/service/booking/RequestRetry.java
@@ -0,0 +1,25 @@
+package com.example.workflow.camunda.service.booking;
+
+import org.camunda.bpm.engine.delegate.BpmnError;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+import java.util.logging.Logger;
+
+public class RequestRetry implements JavaDelegate {
+
+    private final Logger log = Logger.getLogger(RequestRetry.class.getName());
+
+    @Override
+    public void execute(DelegateExecution execution) throws Exception {
+        try{
+            //call gupshup to send message
+            log.info("RequestRetry: execute method is called......");
+            //set relevant variables for future ref
+            execution.setVariable("RequestRetry", true);
+        } catch (Exception e){
+            log.warning("RequestRetry: Exception occured......");
+            throw new BpmnError("booking_flow_error","Error sending message.....");
+        }
+    }
+}
